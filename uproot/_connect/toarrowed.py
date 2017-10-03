b58ec7e93b71bb729ada5ef22755467e0f2dd291
Jim Pivarski <jpivarski@gmail.com>
2017-10-03 14:30:46 -0500

    trouble automatically grouping branches by counter

--- uproot/_connect/toarrowed.py
+++ uproot/_connect/toarrowed.py
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import re
+
 from collections import OrderedDict
 
 from arrowed.oam import PrimitiveOAM
@@ -22,9 +24,59 @@ from arrowed.oam import ListOffsetOAM
 from arrowed.oam import RecordOAM
 from arrowed.oam import PointerOAM
 
-def defineOAM(tree):
-    return None
+import uproot
 
-tree = uproot.open("~/storage/data/nano-TTLHE-2017-09-04-lz4.root")["Events"]
+def defineOAM(tree, rename=lambda x: x.name):
+    class ListWithName(list):
+        def __init__(self, name):
+            super(ListWithName, self).__init__()
+            self.name = name
+
+    def recurse(branch):
+        bycounter = {}
+        for subbranch in branch.branches:
+            if len(subbranch.leaves) == 1:
+                counter = subbranch.leaves[0].counter
+                if counter is not None:
+                    counter = counter.name
+
+                if counter not in bycounter:
+                    bycounter[counter] = ListWithName(subbranch.name)
+
+                if len(subbranch.branches) == 0:
+                    bycounter[counter].append(subbranch)
+                else:
+                    bycounter[counter].append(recurse(subbranch))
+
+        return bycounter
+    
+    structure = recurse(tree)
+
+    def recurse(counter, group):
+        fields = {}
+        for obj in group:
+            if isinstance(obj, dict):
+                for subcounter, subgroup in obj.items():
+                    # no more than one level may be counted
+                    assert counter is None or subcounter is None
+                    if counter is None:
+                        counter = subcounter
+                    fields[subgroup.name] = recurse(counter, subgroup)
+
+            elif hasattr(obj, "dtype"):
+                fields[obj.name] = PrimitiveOAM(obj.name)
+
+        if counter is None:
+            return RecordOAM(fields)
+        else:
+            print "HERE", fields
+
+            return ListCountOAM(counter, RecordOAM(fields))
+
+    return ListCountOAM(None, RecordOAM(dict(recurse(counter, group) for counter, group in structure.items())))
+
+tree = uproot.open("~/storage/data/small-evnt-tree-fullsplit.root")["tree"]
+
+# tree = uproot.open("~/storage/data/nano-TTLHE-2017-09-04-lz4.root")["Events"]
 
 print defineOAM(tree).format()
