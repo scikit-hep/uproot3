8874108ff53f4436928fa5bb95db4448ef5c34e1
Jim Pivarski <jpivarski@gmail.com>
2017-11-08 09:09:34 -0600

    iteration seems to work

--- uproot/jagged.py
+++ uproot/jagged.py
@@ -31,6 +31,7 @@
 import numpy
 try:
     import numba
+    import llvmlite
 except ImportError:
     numba = None
 
@@ -49,11 +50,9 @@ def _jaggedarray_getitem(jaggedarray, index):
         raise IndexError("index out of range for JaggedArray")
 
 class JaggedArray(object):
-    stops_dtype = numpy.dtype(numpy.int64)
-
     def __init__(self, contents, stops):
         assert isinstance(contents, numpy.ndarray)
-        assert isinstance(stops, numpy.ndarray) and stops.dtype == JaggedArray.stops_dtype
+        assert isinstance(stops, numpy.ndarray) and issubclass(stops.dtype.type, numpy.integer)
         assert len(stops.shape) == 1
         self.contents = contents
         self.stops = stops
@@ -61,14 +60,13 @@ class JaggedArray(object):
     def __getitem__(self, index):
         return _jaggedarray_getitem(self, index)
 
-a = JaggedArray(numpy.array([1.1, 1.1, 1.1, 3.3, 3.3]), numpy.array([3, 3, 5]))
-
 class JaggedArrayType(numba.types.Type):
     concrete = {}
 
     def __init__(self, contents, stops):
         assert isinstance(contents, numba.types.Array)
-        assert isinstance(stops, numba.types.Array) and stops.dtype == numba.numpy_support.from_dtype(JaggedArray.stops_dtype) and stops.ndim == 1
+        assert isinstance(stops, numba.types.Array)
+        assert stops.ndim == 1
         self.contents = contents
         self.stops = stops
         if contents.name.startswith("array"):
@@ -134,27 +132,14 @@ class JaggedArray_getitem(numba.typing.templates.AbstractTemplate):
 @numba.extending.lower_builtin("getitem", JaggedArrayType, numba.types.Integer)
 def jaggedarray_getitem(context, builder, sig, args):
     try:
-        getitem_imp = jaggedarray_getitem.cache[sig.args[1]]
-
+        getitem_imp = jaggedarray_getitem.cache[sig.args]
     except KeyError:
         getitem = numba.njit([sig])(_jaggedarray_getitem)
         cres = getitem.overloads.values()[0]
         getitem_imp = cres.target_context.get_function(cres.entry_point, cres.signature)._imp
-        jaggedarray_getitem.cache[sig.args[1]] = getitem_imp
-
+        jaggedarray_getitem.cache[sig.args] = getitem_imp
     return getitem_imp(context, builder, sig, args)
-
 jaggedarray_getitem.cache = {}
-
-# @numba.typing.templates.infer
-# class JaggedArray_getiter(numba.typing.templates.AbstractTemplate):
-#     key = "getiter"
-#     def generic(self, args, kwds):
-#         objtyp, idxtyp = args
-#         if isinstance(objtyp, JaggedArrayType):
-#             idxtyp = numba.typing.builtins.normalize_1d_index(idxtyp)
-#             if isinstance(idxtyp, numba.types.Integer):
-#                 return numba.typing.templates.signature(objtyp.contents, objtyp, idxtyp)
 
 @numba.extending.register_model(JaggedArrayType)
 class JaggedArrayModel(numba.datamodel.models.TupleModel):
@@ -190,20 +175,97 @@ def jaggedarray_box(typ, val, c):
     c.pyapi.decref(class_obj)
     return res
 
-@numba.njit
-def test1(a, i):
-    return a[i]
-print test1(a, 0), a[0]
-print test1(a, 1), a[1]
-print test1(a, 2), a[2]
-print test1(a, -1), a[-1]
-print test1(a, -2), a[-2]
-print test1(a, -3), a[-3]
+class JaggedArrayIteratorType(numba.types.common.SimpleIteratorType):
+    def __init__(self, jaggedarraytype):
+        self.jaggedarray = jaggedarraytype
+        name = "iter({0})".format(jaggedarraytype.name)
+        super(JaggedArrayIteratorType, self).__init__(name, jaggedarraytype.contents)
+
+@numba.datamodel.registry.register_default(JaggedArrayIteratorType)
+class JaggedArrayIteratorModel(numba.datamodel.models.StructModel):
+    def __init__(self, dmm, fe_type):
+        members = [("index", numba.types.EphemeralPointer(numba.types.uintp)),
+                   ("jaggedarray", fe_type.jaggedarray)]
+        super(JaggedArrayIteratorModel, self).__init__(dmm, fe_type, members)
+
+@numba.typing.templates.infer
+class JaggedArray_getiter(numba.typing.templates.AbstractTemplate):
+    key = "getiter"
+    def generic(self, args, kwds):
+        objtyp, = args
+        if isinstance(objtyp, JaggedArrayType):
+            return numba.typing.templates.signature(JaggedArrayIteratorType(objtyp), objtyp)
+    
+@numba.extending.lower_builtin("getiter", JaggedArrayType)
+def jaggedarray_getiter(context, builder, sig, args):
+    arrayty, = sig.args
+    array, = args
+
+    iterobj = context.make_helper(builder, sig.return_type)
+
+    zero = context.get_constant(numba.types.uintp, 0)
+    indexptr = numba.cgutils.alloca_once_value(builder, zero)
+    iterobj.index = indexptr
+    iterobj.jaggedarray = array
+
+    # incref array
+    if context.enable_nrt:
+        context.nrt.incref(builder, arrayty, array)
+
+    res = iterobj._getvalue()
+    return numba.targets.imputils.impl_ret_new_ref(context, builder, sig.return_type, res)
+
+def jaggedarray_getitem_foriter(context, builder, jaggedarraytype, jaggedarray, index):
+    if jaggedarray_getitem_foriter.cache is None:
+        @numba.njit([jaggedarraytype.contents(jaggedarraytype, numba.types.uintp)])
+        def getitem(a, i):
+            if i == 0:
+                start = 0
+            else:
+                start = a.stops[i - 1]
+            stop = a.stops[i]
+            return a.contents[start:stop]
+        cres = getitem.overloads.values()[0]
+        jaggedarray_getitem_foriter.cache = cres.target_context.get_function(cres.entry_point, cres.signature)._imp
+    return jaggedarray_getitem_foriter.cache(context, builder, jaggedarraytype.contents(jaggedarraytype, numba.types.uintp), (jaggedarray, index))
+jaggedarray_getitem_foriter.cache = None
+
+@numba.extending.lower_builtin("iternext", JaggedArrayIteratorType)
+@numba.targets.imputils.iternext_impl
+def jaggedarray_iternext(context, builder, sig, args, result):
+    jaggedarraytype = sig.args[0].jaggedarray
+    iterobj = context.make_helper(builder, sig.args[0], value=args[0])
+
+    stops = builder.extract_value(iterobj.jaggedarray, 1)
+    stopsary = numba.targets.arrayobj.make_array(jaggedarraytype.stops)(context, builder, value=stops)
+    nitems, = numba.cgutils.unpack_tuple(builder, stopsary.shape, count=1)
+
+    index = builder.load(iterobj.index)
+    is_valid = builder.icmp(llvmlite.llvmpy.core.ICMP_SLT, index, nitems)
+    result.set_valid(is_valid)
+
+    with builder.if_then(is_valid):
+        value = jaggedarray_getitem_foriter(context, builder, jaggedarraytype, iterobj.jaggedarray, index)
+        result.yield_(value)
+        nindex = numba.cgutils.increment_index(builder, index)
+        builder.store(nindex, iterobj.index)
+
+a = JaggedArray(numpy.array([1.1, 1.1, 1.1, 3.3, 3.3]), numpy.array([3, 3, 5]))
 
 # @numba.njit
-# def test2(a):
-#     out = 0.0
-#     for ai in a:
-#         out += ai.sum()
-#     return out
-# print test2(a)
+# def test1(a, i):
+#     return a[i]
+# print test1(a, 0), a[0]
+# print test1(a, 1), a[1]
+# print test1(a, 2), a[2]
+# print test1(a, -1), a[-1]
+# print test1(a, -2), a[-2]
+# print test1(a, -3), a[-3]
+
+@numba.njit
+def test2(a):
+    out = 0.0
+    for ai in a:
+        out += ai.sum()
+    return out
+print test2(a)
