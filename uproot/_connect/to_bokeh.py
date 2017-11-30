bf60ecde8f6f438c642342da4c9d30c84657fb2d
Jim Pivarski <jpivarski@gmail.com>
2017-11-30 13:19:57 -0600

    save

--- uproot/_connect/to_bokeh.py
+++ uproot/_connect/to_bokeh.py
@@ -28,6 +28,8 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import threading
+
 import numpy
 
 class TH1Methods_bokeh(object):
@@ -35,16 +37,16 @@ class TH1Methods_bokeh(object):
         self._hist = hist
 
     def plot(self, *args, **kwds):
-        import bokeh
-
-
-import threading
+        from bokeh.plotting import figure
+        p = figure(plot_width=600, plot_height=600)
+        p.patch([6, 7, 8, 7, 3], [1, 2, 3, 4, 5], alpha=0.5, line_width=2)
+        return p
 
 class BokehCanvas(object):
     _ioloop = None
     _instance = None
 
-    def __new__(cls, port=None, address=None):
+    def __new__(cls, port=None):
         if cls._ioloop is None:
             from tornado.ioloop import IOLoop
             cls._ioloop = IOLoop.current()
@@ -56,22 +58,17 @@ class BokehCanvas(object):
             thread.start()
 
         if cls._instance is None:
-            cls._instance = cls._new(port, address)
+            cls._instance = cls._new(port)
 
         elif port is not None and port != cls._instance._server.port:
             if not cls._instance._server._stopped:
                 cls._instance._server.stop()
-            cls._instance = cls._new(port, address)
-
-        elif address is not None and address != cls._instance._server.address:
-            if not cls._instance._server._stopped:
-                cls._instance._server.stop()
-            cls._instance = cls._new(port, address)
+            cls._instance = cls._new(port)
 
         return cls._instance
 
     @classmethod
-    def _new(cls, port, address):
+    def _new(cls, port):
         import bokeh
         import bokeh.application
         import bokeh.application.handlers
@@ -81,8 +78,6 @@ class BokehCanvas(object):
 
         if port is None:
             port = 0
-        if address is None:
-            address = "0.0.0.0"
 
         def newwindow(doc):
             fig = bokeh.plotting.figure()
@@ -91,77 +86,15 @@ class BokehCanvas(object):
             doc.add_root(self._layout)
             self._doc = doc
 
-        print "new server"
         app = bokeh.application.Application(bokeh.application.handlers.function.FunctionHandler(newwindow))
-        self._server = bokeh.server.server.Server({"/": app}, port=port, address=address, io_loop=cls._ioloop)
+        self._server = bokeh.server.server.Server({"/": app}, port=port, io_loop=cls._ioloop)
         self._server.start()
         return self
 
     def __call__(self, plot):
         def update():
-            print "update"
             self._layout.children[0] = plot
         self._doc.add_next_tick_callback(update)
 
-
-
-from bokeh.plotting import figure
-
-canvas = BokehCanvas(port=5000)
-
-p = figure(plot_width=600, plot_height=600)
-p.patch([6, 7, 8, 7, 3], [1, 2, 3, 4, 5], alpha=0.5, line_width=2)
-
-
-
-    # def __init__(self, port=0, address="0.0.0.0"):
-    #     self._port = port
-    #     self._address = address
-    #     super(BokehCanvas, self).__init__()
-    #     self.daemon = True
-
-    #     from tornado.ioloop import IOLoop
-    #     self._loop = IOLoop.current()
-    #     self.start()
-
-    #     from bokeh.server.server import Server
-    #     from bokeh.application import Application
-    #     from bokeh.application.handlers.function import FunctionHandler
-    #     from bokeh.plotting import figure
-    #     from bokeh.layouts import row
-
-    #     print "launch"
-
-    #     self._doc = None
-
-    #     def newwindow(doc):
-    #         print "newwindow"
-    #         fig = figure()
-    #         fig.patch([], [], line_width=0)
-    #         self._layout = row(fig)
-    #         doc.add_root(self._layout)
-    #         self._doc = doc
-
-    #     server = Server({"/": Application(FunctionHandler(newwindow))},
-    #                     port=self._port, address=self._address, io_loop=self._loop)
-
-    #     print "server start"
-    #     server.start()
-
-    # @property
-    # def plot(self):
-    #     return self._layout.children[0]
-
-    # @plot.setter
-    # def plot(self, newplot):
-    #     def update():
-    #         self._layout.children[0] = newplot
-    #     print "update"
-    #     doc = self._doc
-    #     if doc is not None:
-    #         doc.add_next_tick_callback(update)
-
-    # def run(self):
-    #     print "loop thread"
-    #     if not self._loop._running:
-    #         self._loop.start()
+    def show(self):
+        self._server.show("/")
