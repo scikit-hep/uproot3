3fc9d77dd16421a555e62413a703558275d9e5d2
Pratyush Das <reikdas@gmail.com>
2018-04-29 23:35:28 +0530

    Trying to compare values for test

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -141,3 +141,5 @@ class TestIssues(unittest.TestCase):
         f = uproot.open("tests/samples/issue74.root")['Events']
         self.assertTrue(isinstance(y, uproot.rootio.TVector3) for x in f.array('bees.xyzPosition') for y in x)
         self.assertTrue(isinstance(y, uproot.rootio.TVector2) for x in f.array('bees.xyPosition') for y in x)
+        for x in f.array('bees.xyzPosition'):
+            self.assertEqual(x, (1.0, 2.0, -1.0))
