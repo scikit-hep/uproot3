4a1f9160ad07a247715b3d2aff6874761e19b604
Pratyush Das <reikdas@gmail.com>
2018-08-07 12:05:14 -0500

    Refactoring

--- write/TObjString/streamers.py
+++ write/TObjString/streamers.py
@@ -1,7 +1,7 @@
 class TObjStringStreamers(object):
     
-    def __init__(self, pusher, cursor):
-        self.pusher = pusher
+    def __init__(self, sink, cursor):
+        self.sink = sink
         self.cursor = cursor
         
     def write(self):
@@ -9,33 +9,33 @@ class TObjStringStreamers(object):
         cnt = 1073742190
         vers = 5
         packer = ">IH"
-        self.pusher.set_numbers(self.cursor, packer, cnt, vers)
+        self.sink.set_numbers(self.cursor, packer, cnt, vers)
 
         version = 1
         packer = ">h"
-        self.pusher.set_numbers(self.cursor, packer, version)
+        self.sink.set_numbers(self.cursor, packer, version)
 
         fUniqueID =  0
         fBits =  33554432
         packer = ">II"
-        self.pusher.set_numbers(self.cursor, packer, fUniqueID, fBits)
+        self.sink.set_numbers(self.cursor, packer, fUniqueID, fBits)
         
         name = b""
-        self.pusher.set_strings(self.cursor, name)
+        self.sink.set_strings(self.cursor, name)
         size = 1
         packer = ">i"
-        self.pusher.set_numbers(self.cursor, packer, size)
+        self.sink.set_numbers(self.cursor, packer, size)
 
         bcnt =  1073742168
         packer = ">I"
-        self.pusher.set_numbers(self.cursor, packer, bcnt)
+        self.sink.set_numbers(self.cursor, packer, bcnt)
 
         tag =  4294967295
         packer = ">I"
-        self.pusher.set_numbers(self.cursor, packer, tag)
+        self.sink.set_numbers(self.cursor, packer, tag)
 
         cname = b"TStreamerInfo"
-        self.pusher.set_cname(self.cursor, cname)
+        self.sink.set_cname(self.cursor, cname)
         
         
         #TStreamerInfo Streamer
@@ -44,42 +44,42 @@ class TObjStringStreamers(object):
         cnt =  1073742146
         vers =  9
         packer = ">IH"
-        self.pusher.set_numbers(self.cursor, packer, cnt, vers)
+        self.sink.set_numbers(self.cursor, packer, cnt, vers)
 
         cnt =  1073741848
         vers =  1
         packer = ">IH"
-        self.pusher.set_numbers(self.cursor, packer, cnt, vers)
+        self.sink.set_numbers(self.cursor, packer, cnt, vers)
 
         version = 1
         packer = ">h"
-        self.pusher.set_numbers(self.cursor, packer, version)
+        self.sink.set_numbers(self.cursor, packer, version)
 
         fUniqueID = 0
         fBits =  50397184
         packer = ">II"
-        self.pusher.set_numbers(self.cursor, packer, fUniqueID, fBits)
+        self.sink.set_numbers(self.cursor, packer, fUniqueID, fBits)
 
         name = b'TObjString'
         title = b''
-        self.pusher.set_strings(self.cursor, name)
-        self.pusher.set_strings(self.cursor, title)
+        self.sink.set_strings(self.cursor, name)
+        self.sink.set_strings(self.cursor, title)
 
         fCheckSum =  2626570240
         fClassVersion = 1
         packer = ">Ii"
-        self.pusher.set_numbers(self.cursor, packer, fCheckSum, fClassVersion)
+        self.sink.set_numbers(self.cursor, packer, fCheckSum, fClassVersion)
 
         bcnt =  1073742104
         packer = ">I"
-        self.pusher.set_numbers(self.cursor, packer, bcnt)
+        self.sink.set_numbers(self.cursor, packer, bcnt)
 
         tag =  4294967295
         packer = ">I"
-        self.pusher.set_numbers(self.cursor, packer, tag)
+        self.sink.set_numbers(self.cursor, packer, tag)
 
         cname = b"TObjArray"
-        self.pusher.set_cname(self.cursor, cname)
+        self.sink.set_cname(self.cursor, cname)
         
         
         #TObjArray Streamer
@@ -88,34 +88,34 @@ class TObjStringStreamers(object):
         cnt =  1073742086
         vers =  3
         packer = ">IH"
-        self.pusher.set_numbers(self.cursor, packer, cnt, vers)
+        self.sink.set_numbers(self.cursor, packer, cnt, vers)
 
         version = 1
         packer = ">h"
-        self.pusher.set_numbers(self.cursor, packer, version)
+        self.sink.set_numbers(self.cursor, packer, version)
 
         fUniqueID =  0
         fBits =  33554432
         packer = ">II"
-        self.pusher.set_numbers(self.cursor, packer, fUniqueID, fBits)
+        self.sink.set_numbers(self.cursor, packer, fUniqueID, fBits)
 
         name =  b''
-        self.pusher.set_strings(self.cursor, name)
+        self.sink.set_strings(self.cursor, name)
         size =  2
         low =  0
         packer = ">ii"
-        self.pusher.set_numbers(self.cursor, packer, size, low)
+        self.sink.set_numbers(self.cursor, packer, size, low)
 
         bcnt =  1073741941
         packer = ">I"
-        self.pusher.set_numbers(self.cursor, packer, bcnt)
+        self.sink.set_numbers(self.cursor, packer, bcnt)
 
         tag =  4294967295
         packer = ">I"
-        self.pusher.set_numbers(self.cursor, packer, tag)
+        self.sink.set_numbers(self.cursor, packer, tag)
 
         cname =  b"TStreamerBase"
-        self.pusher.set_cname(self.cursor, cname)
+        self.sink.set_cname(self.cursor, cname)
         
         
         #TStreamerBase Streamer
@@ -124,62 +124,62 @@ class TObjStringStreamers(object):
         cnt =  1073741919
         vers =  3
         packer = ">IH"
-        self.pusher.set_numbers(self.cursor, packer, cnt, vers)
+        self.sink.set_numbers(self.cursor, packer, cnt, vers)
         
         
         #TStreamerElement Streamer
         cnt =  1073741909
         vers =  4
         packer = ">IH"
-        self.pusher.set_numbers(self.cursor, packer, cnt, vers)
+        self.sink.set_numbers(self.cursor, packer, cnt, vers)
 
         cnt =  1073741862
         vers =  1
         packer = ">IH"
-        self.pusher.set_numbers(self.cursor, packer, cnt, vers)
+        self.sink.set_numbers(self.cursor, packer, cnt, vers)
 
         version = 1
         packer = ">h"
-        self.pusher.set_numbers(self.cursor, packer, version)
+        self.sink.set_numbers(self.cursor, packer, version)
 
         fUniqueID =  0
         fBits =  50331648
         packer = ">II"
-        self.pusher.set_numbers(self.cursor, packer, fUniqueID, fBits)
+        self.sink.set_numbers(self.cursor, packer, fUniqueID, fBits)
 
         name = b'TObject'
         title = b'Basic ROOT object'
-        self.pusher.set_strings(self.cursor, name)
-        self.pusher.set_strings(self.cursor, title)
+        self.sink.set_strings(self.cursor, name)
+        self.sink.set_strings(self.cursor, title)
 
         fType =  66
         fSize =  0
         fArrayLength =  0
         fArrayDim =  0
         packer = ">iiii"
-        self.pusher.set_numbers(self.cursor, packer, fType, fSize, fArrayLength, fArrayDim)
+        self.sink.set_numbers(self.cursor, packer, fType, fSize, fArrayLength, fArrayDim)
 
         fMaxIndex =  [0, -1877229523, 0, 0, 0]
         packer = ">i"
-        self.pusher.set_array(self.cursor, packer, fMaxIndex)
+        self.sink.set_array(self.cursor, packer, fMaxIndex)
 
         fTypeName =  b'BASE'
-        self.pusher.set_strings(self.cursor, fTypeName)
+        self.sink.set_strings(self.cursor, fTypeName)
 
         fBaseVersion =  1
         packer = ">i"
-        self.pusher.set_numbers(self.cursor, packer, fBaseVersion)
+        self.sink.set_numbers(self.cursor, packer, fBaseVersion)
 
         bcnt =  1073741940
         packer = ">I"
-        self.pusher.set_numbers(self.cursor, packer, bcnt)
+        self.sink.set_numbers(self.cursor, packer, bcnt)
 
         tag =  4294967295
         packer = ">I"
-        self.pusher.set_numbers(self.cursor, packer, tag)
+        self.sink.set_numbers(self.cursor, packer, tag)
 
         cname =  b"TStreamerString"
-        self.pusher.set_cname(self.cursor, cname)
+        self.sink.set_cname(self.cursor, cname)
         
         
         #TStreamerString Streamer
@@ -188,51 +188,51 @@ class TObjStringStreamers(object):
         cnt =  1073741916
         vers =  2
         packer = ">IH"
-        self.pusher.set_numbers(self.cursor, packer, cnt, vers)
+        self.sink.set_numbers(self.cursor, packer, cnt, vers)
         
         
         #TStreamerElement Streamer
         cnt =  1073741910
         vers =  4
         packer = ">IH"
-        self.pusher.set_numbers(self.cursor, packer, cnt, vers)
+        self.sink.set_numbers(self.cursor, packer, cnt, vers)
 
         cnt =  1073741860
         vers =  1
         packer = ">IH"
-        self.pusher.set_numbers(self.cursor, packer, cnt, vers)
+        self.sink.set_numbers(self.cursor, packer, cnt, vers)
 
         version = 1
         packer = ">h"
-        self.pusher.set_numbers(self.cursor, packer, version)
+        self.sink.set_numbers(self.cursor, packer, version)
 
         fUniqueID =  0
         fBits =  50331648
         packer = ">II"
-        self.pusher.set_numbers(self.cursor, packer, fUniqueID, fBits)
+        self.sink.set_numbers(self.cursor, packer, fUniqueID, fBits)
 
         name = b'fString'
         title = b'wrapped TString'
-        self.pusher.set_strings(self.cursor, name)
-        self.pusher.set_strings(self.cursor, title)
+        self.sink.set_strings(self.cursor, name)
+        self.sink.set_strings(self.cursor, title)
 
         fType =  65
         fSize =  24
         fArrayLength =  0
         fArrayDim =  0
         packer = ">iiii"
-        self.pusher.set_numbers(self.cursor, packer, fType, fSize, fArrayLength, fArrayDim)
+        self.sink.set_numbers(self.cursor, packer, fType, fSize, fArrayLength, fArrayDim)
 
         fMaxIndex =  [0, 0, 0, 0, 0]
         packer = ">i"
-        self.pusher.set_array(self.cursor, packer, fMaxIndex)
+        self.sink.set_array(self.cursor, packer, fMaxIndex)
 
         fTypeName =  b'TString'
-        self.pusher.set_strings(self.cursor, fTypeName)
+        self.sink.set_strings(self.cursor, fTypeName)
 
         n =  0
         packer = ">B"
-        self.pusher.set_numbers(self.cursor, packer, n)
+        self.sink.set_numbers(self.cursor, packer, n)
 
-        self.pusher.set_empty_array(self.cursor)
+        self.sink.set_empty_array(self.cursor)
 
--- writer.py
+++ writer.py
@@ -1,15 +1,17 @@
 import numpy
+
+from write.diskarray import DiskArray
 from write.sink import Sink
 from write.cursor import Cursor
 from write.headkey import HeadKey
-from write.TObjString.tobjstring import TObjString
-from write.TObjString.key import Key as StringKey
-from write.diskarray import DiskArray
 from write.header import Header
 from write.begin_key import Begin_Key
 from write.directoryinfo import DirectoryInfo
-from write.TObjString.junkkey import JunkKey
 from write.first_key import First_Key
+
+from write.TObjString.tobjstring import TObjString
+from write.TObjString.key import Key as StringKey
+from write.TObjString.junkkey import JunkKey
 from write.TObjString.streamers import TObjStringStreamers
 
 class Writer(object):
@@ -18,8 +20,8 @@ class Writer(object):
         self.file = DiskArray(filename, shape = (0,), dtype = numpy.uint8)
         self.bytename = filename.encode("utf-8")
         
-        self.pusher = Sink(self.file)
-        self.pointer = Cursor(0)
+        self.sink = Sink(self.file)
+        self.cursor = Cursor(0)
 
         self.objects = []
         self.loc = []
@@ -28,33 +30,33 @@ class Writer(object):
         fCompress = 0 #Constant for now
         self.header = Header(self.bytename, fCompress)
         #self.header.fSeekInfo modified later
-        self.pusher.set_header(self.pointer, self.header)
+        self.sink.set_header(self.cursor, self.header)
 
-        self.pointer = Cursor(self.header.fBEGIN)
-        pointcheck = self.pointer.index
+        self.cursor = Cursor(self.header.fBEGIN)
+        pointcheck = self.cursor.index
         fName = self.bytename
         key = Begin_Key(fName)
-        self.pusher.set_key(self.pointer, key)
+        self.sink.set_key(self.cursor, key)
 
-        key.fKeylen = self.pointer.index - pointcheck
+        key.fKeylen = self.cursor.index - pointcheck
         key.fObjlen = key.fNbytes - key.fKeylen
-        self.pusher.set_key(Cursor(pointcheck), key)
+        self.sink.set_key(Cursor(pointcheck), key)
 
         #Junk
-        self.pusher.set_strings(self.pointer, fName)
+        self.sink.set_strings(self.cursor, fName)
 
-        self.directory_pointcheck = self.pointer.index
+        self.directory_pointcheck = self.cursor.index
         fNbytesKeys = 0
         fNbytesName = self.header.fNbytesName
         self.directory = DirectoryInfo(fNbytesKeys, fNbytesName, 0)
         #Update directory.fSeekKeys later
-        self.pusher.set_directoryinfo(self.pointer, self.directory)
+        self.sink.set_directoryinfo(self.cursor, self.directory)
 
-        self.pointer.skip(30)
+        self.cursor.skip(30)
 
-        self.header.fEND = self.pointer.index
-        self.header.fSeekFree = self.pointer.index
-        self.pusher.set_header(Cursor(0), self.header)
+        self.header.fEND = self.cursor.index
+        self.header.fSeekFree = self.cursor.index
+        self.sink.set_header(Cursor(0), self.header)
 
         self.create()
 
@@ -66,17 +68,17 @@ class Writer(object):
             temp = str(item.string)
             temp = temp.encode("utf-8")
             self.objects.append(temp)
-            self.loc.append(self.pointer.index)
+            self.loc.append(self.cursor.index)
 
-            pointcheck = self.pointer.index
+            pointcheck = self.cursor.index
             junkkey = JunkKey(temp)
-            self.pusher.set_key(self.pointer, junkkey)
-            junkkey.fKeylen = self.pointer.index - pointcheck
+            self.sink.set_key(self.cursor, junkkey)
+            junkkey.fKeylen = self.cursor.index - pointcheck
             junkkey.fNbytes = junkkey.fKeylen + junkkey.fObjlen
-            self.pusher.set_key(Cursor(pointcheck), junkkey)
+            self.sink.set_key(Cursor(pointcheck), junkkey)
 
             stringobject = TObjString(temp)
-            self.pusher.set_object(self.pointer, stringobject)
+            self.sink.set_object(self.cursor, stringobject)
 
             #Streamername
             if "TObjString" not in self.streamers:
@@ -85,61 +87,61 @@ class Writer(object):
         self.create()
 
     def create(self):
-        self.header.fSeekInfo = self.pointer.index
-        self.pusher.set_header(Cursor(0), self.header)
+        self.header.fSeekInfo = self.cursor.index
+        self.sink.set_header(Cursor(0), self.header)
 
-        pointcheck = self.pointer.index
-        key = First_Key(self.pointer, 0)
-        self.pusher.set_key(self.pointer, key)
+        pointcheck = self.cursor.index
+        key = First_Key(self.cursor, 0)
+        self.sink.set_key(self.cursor, key)
 
-        key.fKeylen = self.pointer.index - pointcheck
+        key.fKeylen = self.cursor.index - pointcheck
         key.fNbytes = key.fKeylen + key.fObjlen
-        self.pusher.set_key(Cursor(pointcheck), key)
+        self.sink.set_key(Cursor(pointcheck), key)
 
         self.header.fNbytesInfo = key.fNbytes
-        self.pusher.set_header(Cursor(0), self.header)
+        self.sink.set_header(Cursor(0), self.header)
 
         for x in self.streamers:
             if x == "TObjString":
-                tobjstring = TObjStringStreamers(self.pusher, self.pointer)
+                tobjstring = TObjStringStreamers(self.sink, self.cursor)
                 tobjstring.write()
 
-        fSeekKeys = self.pointer.index
+        fSeekKeys = self.cursor.index
 
-        self.directory.fSeekKeys = self.pointer.index
-        self.pusher.set_directoryinfo(Cursor(self.directory_pointcheck), self.directory)
+        self.directory.fSeekKeys = self.cursor.index
+        self.sink.set_directoryinfo(Cursor(self.directory_pointcheck), self.directory)
 
-        head_key_pointcheck = self.pointer.index
+        head_key_pointcheck = self.cursor.index
         fNbytes = self.directory.fNbytesKeys
         fSeekKey = self.directory.fSeekKeys
         fName = self.bytename
         head_key = HeadKey(fNbytes, fSeekKey, fName)
-        self.pusher.set_key(self.pointer, head_key)
-        head_key_end = self.pointer.index
+        self.sink.set_key(self.cursor, head_key)
+        head_key_end = self.cursor.index
 
         nkeys = len(self.objects)
         packer = ">i"
-        self.pusher.set_numbers(self.pointer, packer, nkeys)
+        self.sink.set_numbers(self.cursor, packer, nkeys)
 
         #TObjString stuff
         for x in range(nkeys):
             key = StringKey(self.objects[x], self.loc[x])
-            pointcheck = self.pointer.index
-            self.pusher.set_key(self.pointer, key)
-            key.fKeylen = self.pointer.index - pointcheck
+            pointcheck = self.cursor.index
+            self.sink.set_key(self.cursor, key)
+            key.fKeylen = self.cursor.index - pointcheck
             key.fNbytes = key.fKeylen + key.fObjlen
-            self.pusher.set_key(Cursor(pointcheck), key)
+            self.sink.set_key(Cursor(pointcheck), key)
 
-        self.header.fEND = self.pointer.index
-        self.header.fSeekFree = self.pointer.index
+        self.header.fEND = self.cursor.index
+        self.header.fSeekFree = self.cursor.index
 
         # Replacing Values
         self.directory.fNbytesKeys = self.header.fEND - fSeekKeys
-        self.pusher.set_directoryinfo(Cursor(self.directory_pointcheck), self.directory)
+        self.sink.set_directoryinfo(Cursor(self.directory_pointcheck), self.directory)
 
         head_key.fNbytes = self.directory.fNbytesKeys
         head_key.fKeylen = head_key_end - head_key_pointcheck
         head_key.fObjlen = head_key.fNbytes - head_key.fKeylen
-        self.pusher.set_key(Cursor(head_key_pointcheck), head_key)
+        self.sink.set_key(Cursor(head_key_pointcheck), head_key)
 
 
