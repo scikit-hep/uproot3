2bac6aea20c7263bdb3f1d90c81dc3719ab2587d
Noah Biederbeck <noah.biederbeck@udo.edu>
2019-01-18 11:57:07 +0100

    Mock http connection using a local file

--- tests/test_http.py
+++ tests/test_http.py
@@ -1,34 +1,60 @@
 import unittest
 import pytest
+import mock
 
 import uproot
 
 from requests.exceptions import HTTPError
 
 
-class Test(unittest.TestCase):
-    url = "http://scikit-hep.org/uproot/examples/HZZ.root"
-    url_auth = "http://scikit-hep.org/uproot/examples/HZZ.root"
-    auth = ("scikit-hep", "uproot")
+FILE = "foriter"
+LOCAL = f"tests/samples/{FILE}.root"
+URL = f"http://scikit-hep.org/uproot/examples/{FILE}.root"
+URL_AUTH = f"http://scikit-hep.org/uproot/authentication/{FILE}.root"
+AUTH = ("scikit-hep", "uproot")
 
+
+def mock_get_local_instead_of_http(url="", headers={}, auth=None, **kwargs):
+    class MockResponse:
+        def __init__(self, status_code):
+            self.status_code = status_code
+            self.content = open(LOCAL, "br").read()
+            self.headers = {"Content-Range": str(len(self.content))}
+
+        def raise_for_status(self):
+            if self.status_code == 401:  # Authentication Error
+                raise HTTPError
+            elif self.status_code == 200:  # Ok
+                pass
+
+    if url == URL:
+        return MockResponse(200)
+    elif url == URL_AUTH and auth == None:
+        return MockResponse(401)
+    elif url == URL_AUTH and auth == AUTH:
+        return MockResponse(200)
+    elif url == URL_AUTH:
+        return MockResponse(401)
+
+
+@mock.patch("requests.get", mock_get_local_instead_of_http)
+class Test(unittest.TestCase):
     def test_no_auth_needed_no_auth(self):
-        f = uproot.open(self.url)
+        f = uproot.open(URL)
         assert type(f) == uproot.rootio.ROOTDirectory
 
     def test_no_auth_needed_with_auth(self):
-        f = uproot.open(self.url, httpsource={"auth": self.auth})
+        f = uproot.open(URL, httpsource={"auth": AUTH})
         assert type(f) == uproot.rootio.ROOTDirectory
 
     def test_auth_needed_no_auth(self):
         with pytest.raises(HTTPError):
-            f = uproot.open(self.url_auth)
-        assert context_manager.exception.response.status_code == 401
+            f = uproot.open(URL_AUTH)
 
     def test_auth_needed_correct_auth(self):
-        f = uproot.open(self.url_auth, httpsource={"auth": self.auth})
+        f = uproot.open(URL_AUTH, httpsource={"auth": AUTH})
         assert type(f) == uproot.rootio.ROOTDirectory
 
     def test_auth_needed_wrong_auth(self):
         with pytest.raises(HTTPError):
-            f = uproot.open(self.url_auth, httpsource={"auth": ("", "")})
-        assert context_manager.exception.response.status_code == 401
+            f = uproot.open(URL_AUTH, httpsource={"auth": ("", "")})
