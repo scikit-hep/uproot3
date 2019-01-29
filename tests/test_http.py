843afbb5ce30e2357b4fbafb895a91a440389d2d
Jim Pivarski <jpivarski@gmail.com>
2019-01-29 15:04:53 -0600

    awkward 0.8.0, uproot-methods 0.4.0, and DIANA-HEP -> IRIS-HEP

--- tests/test_http.py
+++ tests/test_http.py
@@ -1,18 +1,46 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2019, IRIS-HEP
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import unittest
+
 import pytest
 import mock
-
-import uproot
-
 from requests.exceptions import HTTPError
 
+import uproot
 
 FILE = "foriter"
 LOCAL = "tests/samples/{FILE}.root".format(FILE=FILE)
 URL = "http://scikit-hep.org/uproot/examples/{FILE}.root".format(FILE=FILE)
 URL_AUTH = "http://scikit-hep.org/uproot/authentication/{FILE}.root".format(FILE=FILE)
 AUTH = ("scikit-hep", "uproot")
-
 
 def mock_get_local_instead_of_http(url="", headers={}, auth=None, **kwargs):
     class MockResponse:
@@ -36,7 +64,6 @@ def mock_get_local_instead_of_http(url="", headers={}, auth=None, **kwargs):
         return MockResponse(200)
     elif url == URL_AUTH:
         return MockResponse(401)
-
 
 @mock.patch("requests.get", mock_get_local_instead_of_http)
 class Test(unittest.TestCase):
