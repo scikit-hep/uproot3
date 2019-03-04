5e1d270e92389902b209658d112eac0d585b7d6c
Jim Pivarski <jpivarski@gmail.com>
2019-03-04 17:10:38 +0100

    explicitly put install_requires into tests_require

--- setup.py
+++ setup.py
@@ -96,6 +96,8 @@ Reference documentation
 """
     return before + middle + after
 
+dependencies = ["numpy>=1.13.1", "awkward>=0.8.0", "uproot-methods>=0.4.0", "cachetools"]
+
 setup(name = "uproot",
       version = get_version(),
       packages = find_packages(exclude = ["tests"]),
@@ -110,9 +112,9 @@ setup(name = "uproot",
       download_url = "https://github.com/scikit-hep/uproot/releases",
       license = "BSD 3-clause",
       test_suite = "tests",
-      install_requires = ["numpy>=1.13.1", "awkward>=0.8.0", "uproot-methods>=0.4.0", "cachetools"],
+      install_requires = dependencies,
       setup_requires = ["pytest-runner"],
-      tests_require = ["pytest>=3.9", "pkgconfig", "lz4", 'backports.lzma;python_version<"3.3"', "mock", "requests"],
+      tests_require = dependencies + ["pytest>=3.9", "pkgconfig", "lz4", 'backports.lzma;python_version<"3.3"', "mock", "requests"],
       classifiers = [
           "Development Status :: 5 - Production/Stable",
           "Intended Audience :: Developers",
