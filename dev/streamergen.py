5ed5871167d513aaff6e36cc039aabed53f45652
Pratyush Das <reikdas@gmail.com>
2019-03-14 19:40:11 +0530

    Fix script for changed directory

--- dev/streamergen.py
+++ dev/streamergen.py
@@ -28,9 +28,11 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+# Execute script from root directory of project.
+
 import sys
 import os
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath(""))
 
 import uproot
 
