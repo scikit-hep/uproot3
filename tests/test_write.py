9d44299c5fbdb9f1459ed4a9c0009c952e4a8ba1
Pratyush Das <reikdas@gmail.com>
2019-04-12 20:51:11 +0530

    Test finally rigid enough

--- tests/test_write.py
+++ tests/test_write.py
@@ -31,6 +31,36 @@ def test_cycle(tmp_path):
     assert str(f.Get("hello;2")) == "uproot"
     f.Close()
 
+def test_th1(tmp_path):
+    filename = join(str(tmp_path), "example.root")
+    testfile = join(str(tmp_path), "test.root")
+
+    f = ROOT.TFile.Open(testfile, "RECREATE")
+    h = ROOT.TH1F("hvar", "title", 5, 1, 10)
+    h.Sumw2()
+    h.Fill(1.0, 3)
+    h.Fill(2.0, 4)
+    h.Write()
+    f.Close()
+
+    t = uproot.open(testfile)
+    hist = t["hvar"]
+    with uproot.recreate(filename) as f:
+        f["test"] = hist
+
+    f = ROOT.TFile.Open(filename)
+    h = f.Get("test")
+    sums = [0.0, 25.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    bincontents = [7.0, 0.0, 0.0, 0.0, 0.0]
+    assert list(h.GetSumw2()) == sums
+    count = 0
+    for x in range(1, 6):
+        assert h.GetBinContent(x) == bincontents[count]
+        count += 1
+    assert h.GetNbinsX() == 5
+    assert h.GetMean() == 1.5714285714285714
+    assert h.GetRMS() == 0.4948716593053938
+
 def test_th1_varbin(tmp_path):
     filename = join(str(tmp_path), "example.root")
     testfile = join(str(tmp_path), "test.root")
@@ -39,7 +69,7 @@ def test_th1_varbin(tmp_path):
     bins = np.array([1.0, 3.0, 4.0, 10.0, 11.0, 12.0], dtype="float64")
     f = ROOT.TFile.Open(testfile, "RECREATE")
     h = ROOT.TH1F("hvar", "title", 5, bins)
-    f.Write()
+    h.Write()
     f.Close()
 
     t = uproot.open(testfile)
@@ -76,10 +106,13 @@ def test_th2(tmp_path):
     f = ROOT.TFile.Open(filename)
     h = f.Get("test")
     sums = [0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 9.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 16.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0]
+    bincontents = [0.0, 3.0, 4.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
     count = 0
-    for sum in h.GetSumw2():
-        assert sum == sums[count]
-        count += 1
+    for x in range(1, 6):
+        for y in range(1, 7):
+            assert h.GetBinContent(x, y) == bincontents[count]
+            count += 1
+    assert list(h.GetSumw2()) == sums
     assert h.GetMean() == 1.5714285714285714
     assert h.GetRMS() == 0.4948716593053938
     assert h.GetNbinsX() == 5
@@ -94,7 +127,7 @@ def test_th2_varbin(tmp_path):
     binsy = np.array([1.0, 3.0, 4.0, 10.0, 11.0, 12.0, 20.0], dtype="float64")
     f = ROOT.TFile.Open(testfile, "RECREATE")
     h = ROOT.TH2F("hvar", "title", 5, binsx, 6, binsy)
-    f.Write()
+    h.Write()
     f.Close()
 
     t = uproot.open(testfile)
