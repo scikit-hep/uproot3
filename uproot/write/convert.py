fd1739e049696e5efdc19f3cde97bac8a8bd9f53
Pratyush Das <reikdas@gmail.com>
2019-06-28 10:48:06 -0500

    Added simple leaf types

--- uproot/write/convert.py
+++ uproot/write/convert.py
@@ -1,4 +1,9 @@
-import uproot.tree as tree_reader
+#!/usr/bin/env python
+
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot/blob/master/LICENSE
+
+import numpy
+
 import uproot
 import uproot.write.objects.TTree
 
@@ -19,10 +24,34 @@ def ttree(tree):
             name = branch._fName
             title = branch._fTitle
             if branch._fLeaves[0].__class__.__name__ == "TLeafI":
-                dtype = "int"
+                dtype = numpy.dtype("int32")
             elif branch._fLeaves[0].__class__.__name__ == "TLeafB":
-
-            name = uproot.write.objects.TTree.branch()
+                dtype = numpy.dtype("int8")
+            elif branch._fLeaves[0].__class__.__name__ == "TLeafC":
+                dtype = numpy.dtype("S") #Variable length strings?
+            elif branch._fleaves[0].__class__.__name__ == "TLeafD":
+                dtype = numpy.dtype("float64")
+            elif branch._fLeaves[0].__class__.__name__ == "TLeafD32":
+            #   dtype = numpy.dtype("float24") #Truncated 24 bit floating point - Double32
+                raise NotImplementedError
+            elif branch._fLeaves[0].__class__.__name__ == "TLeafElement":
+            #   dtype = ....
+                raise NotImplementedError
+            elif branch._fLeaves[0].__class__.__name__ == "TLeafF":
+                dtype = numpy.dtype("float32")
+            elif branch._fLeaves[0].__class__.__name__ == "TLeafF16":
+                dtype = numpy.dtype("float16")
+            elif branch._fLeaves[0].__class__.__name__ == "TLeafL":
+                dtype = numpy.dtype("int64")
+            elif branch._fLeaves[0].__class__.__name__ == "TLeafO":
+                dtype = numpy.dtype("bool")
+            elif branch._fLeaves[0].__class__.__name__ == "TLeafS":
+                dtype = numpy.dtype("int16")
+            elif branch._fLeaves[0].__class__.__name__ == "TLeafObject":
+            #   dtype = ...
+                raise NotImplementedError
+            name = uproot.write.objects.TTree.branch(dtype, name, title)
+            # Have to add branch attributes
 
         ttree = uproot.write.objects.TTree.TTree(fixstring(tree._fName), fixstring(tree._fTitle), branches)
         ttree.fields = ttree.emptyfields()
