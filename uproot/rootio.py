b033536e44cc66092d328333926452e656f8e919
Jim Pivarski <jpivarski@gmail.com>
2019-09-05 10:01:08 -0500

    Actually, TRefArray shouldn't descend from TArray in uproot because it doesn't in ROOT.

--- uproot/rootio.py
+++ uproot/rootio.py
@@ -1350,6 +1350,28 @@ TRef._methods = TRef
 TRef._arraymethods = None
 TRef._fromrow = lambda row: TRef(row["id"])
 
+class TRefArray(list, ROOTStreamedObject):
+    _format1 = struct.Struct(">i")
+    _dtype = numpy.dtype(">i4")
+
+    @classmethod
+    def _readinto(cls, self, source, cursor, context, parent):
+        start, cnt, self._classversion = _startcheck(source, cursor)
+        cursor.skip(10)
+        self.name = cursor.string(source)
+        self.length = cursor.field(source, self._format1)
+        cursor.skip(6)
+        self.extend(cursor.array(source, self.length, self._dtype))
+        _endcheck(start, cursor, cnt)
+        return self
+
+    @property
+    def nbytes(self):
+        return len(self) * self._dtype.itemsize
+
+    def tostring(self):
+        return numpy.asarray(self, dtype=self._dtype).tostring()
+
 class TArray(list, ROOTStreamedObject):
     @classmethod
     def _readinto(cls, self, source, cursor, context, parent):
@@ -1385,20 +1407,6 @@ class TArrayF(TArray):
 
 class TArrayD(TArray):
     _dtype = numpy.dtype(">f8")
-
-class TRefArray(TArrayI):
-    _format1 = struct.Struct(">i")
-
-    @classmethod
-    def _readinto(cls, self, source, cursor, context, parent):
-        start, cnt, self._classversion = _startcheck(source, cursor)
-        cursor.skip(10)
-        self.name = cursor.string(source)
-        self.length = cursor.field(source, self._format1)
-        cursor.skip(6)
-        self.extend(cursor.array(source, self.length, self._dtype))
-        _endcheck(start, cursor, cnt)
-        return self
 
 # FIXME: I want to generalize this. It's the first example of a class that doesn't
 # follow the usual pattern. The full 11 bytes are
