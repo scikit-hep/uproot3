67e01f6a05866d9648c5188291b431fa58654656
Mason Proffitt <mproffit@cern.ch>
2019-10-17 15:21:32 +0200

    update tests for #376

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -214,11 +214,19 @@ class Test(unittest.TestCase):
         assert obj._n == 1
         assert obj._energy[0] == numpy.array([2.3371024], dtype=numpy.float32)[0]
 
-    def test_issue376(self):
+    def test_issue376_simple(self):
         f = uproot.open("tests/samples/from-geant4.root")
         assert type(f).classname() == 'TDirectory'
         assert f.classname() == 'TDirectory'
         real_class_names = ['TTree'] * 4 + ['TH1D'] * 10 + ['TH2D'] * 5
-        assert f.classnames() == real_class_names
-        assert [twotuple[1].classname() for twotuple in f.classes()] == real_class_names
+        assert [classname_two_tuple[1] for classname_two_tuple in f.classnames()] == real_class_names
+        assert [class_two_tuple[1].classname() for class_two_tuple in f.classes()] == real_class_names
         assert [value.classname() for value in f.values()] == real_class_names
+
+    def test_issue376_nested(self):
+        f = uproot.open("tests/samples/nesteddirs.root")
+        top_level_class_names = ['TDirectory', 'TDirectory']
+        recursive_class_names = ['TDirectory', 'TDirectory', 'TTree', 'TTree', 'TDirectory', 'TTree']
+        assert [classname_two_tuple[1] for classname_two_tuple in f.classnames(recursive=False)] == top_level_class_names
+        assert [classname_two_tuple[1] for classname_two_tuple in f.classnames(recursive=True)] == recursive_class_names
+        assert [classname_two_tuple[1] for classname_two_tuple in f.allclassnames()] == recursive_class_names
