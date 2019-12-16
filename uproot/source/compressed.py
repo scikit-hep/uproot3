7c4feb48eaf6117809a9b4293db3fdc227f6ac7f
Jim Pivarski <jpivarski@gmail.com>
2019-12-16 10:07:04 -0600

    Corrections: no conda channels at all. Also, I bumped the version number for later deployment.

--- uproot/source/compressed.py
+++ uproot/source/compressed.py
@@ -66,7 +66,7 @@ class Compression(object):
                 try:
                     from backports.lzma import decompress as lzma_decompress
                 except ImportError:
-                    raise ImportError("Install lzma package with:\n    pip install backports.lzma\nor\n    conda install -c conda-forge backports.lzma\n(or just use Python >= 3.3).")
+                    raise ImportError("Install lzma package with:\n    pip install backports.lzma\nor\n    conda install backports.lzma\n(or just use Python >= 3.3).")
             return lzma_decompress(cursor.bytes(source, compressedbytes))
 
         elif self.algo == uproot.const.kOldCompressionAlgo:
@@ -76,7 +76,7 @@ class Compression(object):
             try:
                 from lz4.block import decompress as lz4_decompress
             except ImportError:
-                raise ImportError("Install lz4 package with:\n    pip install lz4\nor\n    conda install -c conda-forge lz4")
+                raise ImportError("Install lz4 package with:\n    pip install lz4\nor\n    conda install lz4")
 
             if uncompressedbytes is None:
                 raise ValueError("lz4 needs to know the uncompressed number of bytes")
@@ -86,7 +86,7 @@ class Compression(object):
             try:
                 from zstd import decompress as zstd_decompress
             except ImportError:
-                raise ImportError("Install zstd package with:\n    pip install zstd\nor\n    conda install -c anaconda python-zstd")
+                raise ImportError("Install zstd package with:\n    pip install zstd\nor\n    conda install python-zstd")
             return zstd_decompress(cursor.bytes(source, compressedbytes))
 
         else:
@@ -140,7 +140,7 @@ class CompressedSource(uproot.source.source.Source):
                     try:
                         import xxhash
                     except ImportError:
-                        raise ImportError("Install xxhash package with:\n    pip install xxhash\nor\n    conda install -c conda-forge python-xxhash")
+                        raise ImportError("Install xxhash package with:\n    pip install xxhash\nor\n    conda install python-xxhash")
                     compression = self.compression.copy(uproot.const.kLZ4)
                     compressedbytes -= 8
                     checksum = cursor.field(self._compressed, self._format_field0)
